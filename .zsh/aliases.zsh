4f3814727eaaf38f694634b8a6e26c593c7f1271
Christophe Sicard <sicard.christophe@gmail.com>
2015-04-04 07:45:23 +0200

    Zsh configuration

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,22 @@
+alias dk="docker"
+alias dc="docker-compose"
+alias de="docker-enter"
+alias dk-rmi-none="docker images | grep none | awk '{print $3}' | xargs docker rmi"
+
+alias vup="vagrant up"
+alias vhalt="vagrant halt"
+alias vssh="vagrant ssh"
+
+alias find-~="find -name '*~'"
+alias find-sw="find -regex '.*\.sw[p-q]'"
+
+#alias grep='grep --exclude="*~"'
+alias grepphp='grep -R --color -n --include="*.php"'
+alias grepctp='grep -R --color -n --include="*.ctp"'
+alias grepjson='grep -R --color -n --include="*.json"'
+alias grepjs='grep -R --color -n --include="*.js"'
+alias grepcss='grep -R --color -n --include="*.css"'
+alias grepxml='grep -R --color -n --include="*.xml"'
+
+alias brightness='sudo /home/christophe/bin/brightness'
+alias vim='vim --servername local'
---
+++ .zsh/powerline.zsh
@@ -0,0 +1,1 @@
+source /usr/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
